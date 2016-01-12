e3a119ca471577cb394b72c3a97cb776710f35a4
Andrew Mager <andrew+github@mager.co>
2016-01-11 18:03:24 -0800

    body-parser

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser')
 
+var app = express();
 app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
 
