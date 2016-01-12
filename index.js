1fffddec072449d40fa270f90bae0968c94565d5
Andrew Mager <andrew+github@mager.co>
2016-01-11 18:11:45 -0800

    No errors

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+app.route('/')
+  .get(function(req, res) {
+    response.render('pages/index');
+  })
+  .post(function(req, res) {
+    console.log(req);
+    res.send('Success');
+  });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
